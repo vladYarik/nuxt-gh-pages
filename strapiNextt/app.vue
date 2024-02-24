a54d2ac8488a7f80f172e1422bc2261ac794b965
kartoshka1753 <kartoshka1753@mail.ru>
2024-02-24 21:50:54 +0600

    dc

---
+++ strapiNextt/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="data">
+    {{data.data}}
+  </div>
+  <div v-else>
+    dfv
+  </div>
+</template>
+
+<script setup>
+  import {useFetch} from "nuxt/app";
+
+  const fetchUser = async () => {
+      return new Promise(resolve => {
+        setTimeout(async () => {
+         resolve({
+           data:'dfvfdfv'
+         })
+        },6000)
+      })
+
+    // const a = await fetch('http://localhost:1337/api/contents')
+    // const data = await a.json()
+    // return data.data[0].attributes
+
+
+  }
+
+  const {data} = await useFetch(async () => {
+      return fetchUser()
+
+
+  })
+  console.log(data.value)
+</script>
