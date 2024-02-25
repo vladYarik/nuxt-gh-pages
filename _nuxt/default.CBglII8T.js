9c260b2dfa0f402d130a4d6bbe09dd501d428129
kartoshka1753 <kartoshka1753@mail.ru>
2024-02-25 11:23:14 +0600

    Updates

---
+++ _nuxt/default.CBglII8T.js
@@ -0,0 +1,1 @@
+import{_ as t,t as s,v as a,x as o,L as c}from"./entry.7fIZGCgJ.js";const r={name:"default"},d={class:"layout"},n={class:"wrapper"};function _(e,l,p,f,u,i){return s(),a("div",d,[o("div",n,[c(e.$slots,"default",{},void 0,!0)])])}const $=t(r,[["render",_],["__scopeId","data-v-e12e225c"]]);export{$ as default};
